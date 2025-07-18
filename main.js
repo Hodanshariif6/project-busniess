a77f57b0605bf36cee999eb38f0cde9980a42ba8
hodanshariif6 <hodanshariif617@gmail.com>
2025-07-17 21:27:41 -0700

    test

---
+++ main.js
@@ -0,0 +1,43 @@
+const btn1 = document.querySelector("#btn1")
+const btn2 = document.querySelector("#btn2")
+const btn3 = document.querySelector("#btn3")
+const btn4 = document.querySelector("#btn4")
+const btn5 = document.querySelector("#btn5")
+const btn6 = document.querySelector("#btn6")
+const btn7 = document.querySelector("#btn7")
+const btn8 = document.querySelector("#btn8")
+const menu = document.querySelector("#menu")
+
+let startValue = ""
+btn1.addEventListener("click", () => {
+    startValue ++
+    menu.innerHTML = startValue
+})
+btn2.addEventListener("click", () => {
+    startValue ++
+    menu.innerHTML = startValue
+})
+btn3.addEventListener("click", () => {
+    startValue ++
+    menu.innerHTML = startValue
+})
+btn4.addEventListener("click", () => {
+    startValue ++
+    menu.innerHTML = startValue
+})
+btn5.addEventListener("click", () => {
+    startValue ++
+    menu.innerHTML = startValue
+})
+btn6.addEventListener("click", () => {
+    startValue ++
+    menu.innerHTML = startValue
+})
+btn7.addEventListener("click", () => {
+    startValue ++
+    menu.innerHTML = startValue
+})
+btn8.addEventListener("click", () => {
+    startValue ++
+    menu.innerHTML = startValue
+})
